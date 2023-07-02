d32235d5fc1ebd0609aef56cd9a8bd688cc6ccfb
HamzaIqbal69 <hamza.n.iqbal@gmail.com>
2023-07-02 09:04:34 -0400

    Started filling stuff in, still need to figure out values for macros

---
+++ Drivers/CAN/can_config.h
@@ -0,0 +1,32 @@
+/**
+ * @file can_config.h
+ * @author Hamza Iqbal 
+ * @brief Configuration File for CAN driver
+ * 
+ */
+
+
+#define CAN_BAUD_RATE                       1000000     //Change if needed
+#define CAN_PRESCALER               
+#define CAN_MODE
+#define CAN_SYNC_JUMP_WIDTH
+#define CAN_TIME_SEG_1
+#define CAN_TIME_SEG_2
+#define CAN_TIME_TRIGGERED_MODE
+#define CAN_AUTO_BUS_OFF
+#define CAN_AUTO_WAKEUP
+#define CAN_AUTO_RETRANSMISSION
+#define CAN_RECEIVE_FIFO_LOCKED
+#define CAN_TRANSMIT_FIFO_PRIORITY
+#define CAN_FILTER_ID_HIGH
+#define CAN_FILTER_ID_LOW
+#define CAN_MASK_ID_HIGH
+#define CAN_MASK_ID_LOW
+#define CAN_FILTER_MASK_ID_HIGH
+#define CAN_FILTER_MASK_ID_LOW
+#define CAN_FILTER_FIFO_ASSIGNMENT
+#define CAN_FILTER_BANK
+#define CAN_FILTER_MODE
+#define CAN_FILTER_SCALE
+#define CAN_FILTER_ACTIVATION
+#define CAN_SLAVE_START_FLITER_BANK
