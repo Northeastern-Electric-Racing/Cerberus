0417dfa8ced037de65ee8b5aace7fb7019963cdc
HamzaIqbal69 <hamza.n.iqbal@gmail.com>
2023-07-19 22:02:09 -0700

    made eeprom driver skeleton

---
+++ Drivers/NER/include/m24c32.h
@@ -0,0 +1,21 @@
+#ifndef M24C32_H
+#define M24C32_H
+
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_i2c.h"
+
+#define M24C32_I2C_ADDR     0x50
+#define M24C32_NUM_PAGE     250
+#define M24C32_PAGE_SIZE    32
+
+void EEPROM_Write(uint8_t page, uint16_t offset, uint8_t *data, uint16_t size);
+
+void EEPROM_Read(uint8_t page, uint16_t offset, uint8_t *data, uint16_t size);
+
+void EEPROM_Delete(uint8_t page, uint16_t offset, uint16_t size);
+
+void EEPROM_Page_Erase(uint8_t page);
+
+
+
+#endif
---
+++ Drivers/NER/src/m24c32.c
@@ -0,0 +1,26 @@
+#include "m24c32.h"
+
+uint16_t bytes_to_write(uint16_t size, uint16_t offset)
+{
+    return;
+}
+
+void EEPROM_Write(uint8_t page, uint16_t offset, uint8_t *data, uint16_t size)
+{
+    return;
+}
+
+void EEPROM_Read(uint8_t page, uint16_t offset, uint8_t *data, uint16_t size)
+{
+    return;
+}
+
+void EEPROM_Delete(uint8_t page, uint16_t offset, uint16_t size)
+{
+
+}
+
+void EEPROM_Page_Erase(uint8_t page)
+{
+    return;
+}
