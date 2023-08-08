e397daaebe7422865a26e8993b64bb58741f1499
HamzaIqbal69 <hamza.n.iqbal@gmail.com>
2023-08-07 21:21:23 -0700

    Added stuff to eeprom driver

--- Drivers/NER/include/m24c32.h
+++ Drivers/NER/include/m24c32.h
@@ -8,6 +8,8 @@
 #define M24C32_NUM_PAGE     250
 #define M24C32_PAGE_SIZE    32
 
+I2C_HandleTypeDef *i2c_handle;
+
 void EEPROM_Write(uint8_t page, uint16_t offset, uint8_t *data, uint16_t size);
 
 void EEPROM_Read(uint8_t page, uint16_t offset, uint8_t *data, uint16_t size);
--- Drivers/NER/src/m24c32.c
+++ Drivers/NER/src/m24c32.c
@@ -2,17 +2,54 @@
 
 uint16_t bytes_to_write(uint16_t size, uint16_t offset)
 {
-    return;
+    if((size + offset) < M24C32_PAGE_SIZE) return size;
+    else return M24C32_PAGE_SIZE - offset;
 }
 
 void EEPROM_Write(uint8_t page, uint16_t offset, uint8_t *data, uint16_t size)
 {
-    return;
+    uint8_t start_page = page;
+    uint8_t end_page = page + ((size + offset) / M24C32_PAGE_SIZE)
+    uint8_t num_pages = end_page - start_page + 1;
+
+    uint8_t data_pos = 0;
+
+    for(int i = 0; i < num_pages; i++)
+    {
+        uint16_t mem_addr = start_page << 4 | offset;
+        uint16_t bytes_remaining = bytes_to_write(size, offset);
+
+        HAL_I2C_Mem_Write(i2c_handle, M24C32_I2C_ADDR, mem_addr, 2, &data[data_pos], bytes_remaining, 1000);
+        start_page += 1;
+        offset = 0;
+        size = size - bytes_remaining;
+        data_pos = bytes_remaining;
+
+        HAL_Delay(5);
+    }
 }
 
 void EEPROM_Read(uint8_t page, uint16_t offset, uint8_t *data, uint16_t size)
 {
-    return;
+    uint8_t start_page = page;
+    uint8_t end_page = page + ((size + offset) / M24C32_PAGE_SIZE)
+    uint8_t num_pages = end_page - start_page + 1;
+
+    uint8_t data_pos = 0;
+
+    for(int i = 0; i < num_pages; i++)
+    {
+        uint16_t mem_addr = start_page << 4 | offset;
+        uint16_t bytes_remaining = bytes_to_write(size, offset);
+
+        HAL_I2C_Mem_Read(i2c_handle, M24C32_I2C_ADDR, mem_addr, 2, &data[data_pos], bytes_remaining, 1000);
+        start_page += 1;
+        offset = 0;
+        size = size - bytes_remaining;
+        data_pos = bytes_remaining;
+
+        HAL_Delay(5);
+    }
 }
 
 void EEPROM_Delete(uint8_t page, uint16_t offset, uint16_t size)
