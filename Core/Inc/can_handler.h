7ecfc5a35b5beef081b5f80131cd073c17a8de1e
nwdepatie <nwdepatie@gmail.com>
2023-10-07 04:04:55 +0000

    Fixed some weird stuff and added RTOS queues

--- Core/Inc/can_handler.h
+++ Core/Inc/can_handler.h
@@ -16,69 +16,8 @@
 
 #include "cmsis_os.h"
 
-#define NUM_CALLBACKS 5 // Update when adding new callbacks
-
-/* Used in the Queue implementation - you probably dont need to worry about it
- */
-struct node {
-	can_msg_t msg;
-	struct node* next;
-};
-
-/* This is a queue of messages that are waiting for processing by your
- * application code */
-struct msg_queue {
-	struct node* head;
-	struct node* tail;
-};
-
-struct FunctionInfo can_callbacks[] = { 
-	({ 0x2010, void (*MC_update)(can_msg_t) }),
-	({ 0x2110, void (*MC_update)(can_msg_t) }),
-	({ 0x2210, void (*MC_update)(can_msg_t) }),
-	({ 0x2310, void (*MC_update)(can_msg_t) }),
-	({ 0x2410, void (*MC_update)(can_msg_t) }) }
-
-/* Struct to couple function with message IDs */
-typedef struct
-{
-	uint8_t messageID;
-	void (*function)(void);
-} FunctionInfo;
-
-/* Hashmap node structure */
-typedef struct HashNode {
-	FunctionInfo info;
-	struct HashNode* next;
-} HashNode;
-
-/* Hashmap structure */
-typedef struct
-{
-	HashNode* array[MAX_MAP_SIZE];
-} HashMap;
-
-/* These are the queues for each CAN line */
-struct msg_queue* can1_incoming;
-struct msg_queue* can2_incoming;
-// struct msg_queue* can3_incoming;
-struct msg_queue* can1_outgoing;
-struct msg_queue* can2_outgoing;
-// struct msg_queue* can3_outgoing;
-
-struct HashMap callback_map;
-
-void can_handler_init();
-
-static void enqueue(struct msg_queue* queue, can_msg_t msg);
-
-static can_msg_t* dequeue(struct msg_queue* queue);
-
 void vRouteCanIncoming(void* pv_params);
-
-osThreadId_t route_can_incoming_handle;
-const osThreadAttr_t route_can_incoming_attributes = { .name = RouteCanIncoming,
-	.stack_size = 128 * 8,
-	.priority = (osPriority_t)osPriorityAboveNormal4 };
+extern osThreadId_t route_can_incoming_handle;
+extern const osThreadAttr_t route_can_incoming_attributes;
 
 #endif
