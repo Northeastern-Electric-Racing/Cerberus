81f2fe70a2b64c7da7d2b4db4132f28167e46460
Rand-Sai <yuan.sa@northeastern.edu>
2023-10-14 18:39:04 -0400

    Modified LSM6DSO_IMU.cs

--- LSM6DSO_IMU_I2C.cs
+++ LSM6DSO_IMU_I2C.cs
@@ -115,6 +115,71 @@ namespace Antmicro.Renode.Peripherals.Sensors
         {
             this.Log(LogLevel.Noisy, "Finishing transmission, going to the Idle state");
             commandInProgress = CommandTypes.None;
+            state = State.Idle;
+        }
+
+        public void Write(byte[] data)
+        {
+            this.Log(LogLevel.Noisy, "Written {0} bytes: {1}", data.Length, Misc.PrettyPrintCollectionHex(data));
+             foreach(var b in data)
+            {
+                WriteByte(b);
+            }         
+        }
+
+        public void WriteByte(byte b)
+        {
+            switch(state)
+            {
+                case State.Idle:
+                    address = BitHelper.GetValue(b, offset: 0, size: 7);
+                    this.Log(LogLevel.Noisy, "Setting register address to {0} (0x{0:X})", (Registers)address);
+                    state = State.Processing;
+                    break;
+
+                case State.Processing:
+                    this.Log(LogLevel.Noisy, "Writing value 0x{0:X} to register {1} (0x{1:X})", b, (Registers)address);
+                    RegistersCollection.Write(address, b);
+                    TryIncrementAddress();
+                    break;
+
+                default:
+                    throw new ArgumentException($"Unexpected state: {state}");
+            }
+        }
+
+        public byte[] Read(int count = 1)
+        {
+            var dequeued = false;
+            switch(address)
+            {
+                case (byte)Registers.AccelerometerXLow:
+                    dequeued = commonFifo.TryDequeueNewSample();
+                    break;
+
+                case (byte)Registers.GyroscopeXLow:
+                    dequeued = commonFifo.TryDequeueNewSample();
+                    break;
+
+                case (byte)Registers.TemperatureLow:
+                    // temperature data is not queued
+                    commonFifo.TryDequeueNewSample();
+                    break;
+            }
+
+            if(dequeued)
+            {
+                if(currentReportedFifoDepth > 0)
+                {
+                    currentReportedFifoDepth--;
+                }
+            }
+
+            var result = RegistersCollection.Read(address);
+            this.NoisyLog("Reading register {1} (0x{1:X}) from device: 0x{0:X}", result, (Registers)address);
+            TryIncrementAddress();
+
+            return new byte [] { result };
         }
         
         public override void Reset()
@@ -133,6 +198,7 @@ namespace Antmicro.Renode.Peripherals.Sensors
 
             temperatureResdStream?.Dispose();
             temperatureResdStream = null;
+            state = State.Idle;
         }
 
         public byte Transmit(byte data)
@@ -240,7 +306,7 @@ namespace Antmicro.Renode.Peripherals.Sensors
                         {
                             return 0u;
                         }
-                        DebugAssert(sample.IsAccelerationSample || sample.IsAngularRateSample, $"Invalid sample: {sample}");
+                        // Debug.Assert(sample.IsAccelerationSample || sample.IsAngularRateSample, $"Invalid sample: {sample}");
 
                         var sensitivity = sample.IsAccelerationSample ? accelerationSensitivity : angularRateSensitivity;
                         var _byte = sample.GetScaledValueByte(registerInfo.Axis, sensitivity, registerInfo.UpperByte, out var realScaledValue);
@@ -614,7 +680,9 @@ namespace Antmicro.Renode.Peripherals.Sensors
         private IManagedThread gyroFeederThread;
 
         private decimal accelerationSensitivity;
+        private uint currentReportedFifoDepth;        
         private byte address;
+        private State state;
         private decimal angularRateSensitivity;
         private CommandTypes commandInProgress;
         private bool previousFifoOverrunStatus;
@@ -1080,6 +1148,11 @@ namespace Antmicro.Renode.Peripherals.Sensors
             SensorHubNack = 0x19
         }
 
+        private enum State
+        {
+            Idle,
+            Processing
+        }
         private enum Registers : byte
         {
             RegistersAccessConfiguration = 0x01,
