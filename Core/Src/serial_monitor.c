f732afec67c72681e7085b6d640b58d3ff2e86f0
Nicholas DePatie <80368116+nwdepatie@users.noreply.github.com>
2023-11-05 17:50:51 -0500

    Created a very basic task for handling outbound UART messages (#66)
    * Created a very basic task for handling outbound UART messages, still needs a lot of work
    
    * Adding task into main
    
    * Copying ESP32's implementation which should work for our case
    
    * Got basic UART terminal working with renode
    
    * Adding tmux to docker container, makes it easier to debug serial
    
    * trying to make printf redirection work
    
    * Printf routing task works!

---
+++ Core/Src/serial_monitor.c
@@ -0,0 +1,53 @@
+#include "serial_monitor.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+#define PRINTF_QUEUE_SIZE 16  /* Strings */
+#define PRINTF_BUFFER_LEN 128 /* Characters */
+
+osMessageQueueId_t printf_queue;
+osThreadId_t serial_monitor_handle;
+const osThreadAttr_t serial_monitor_attributes;
+
+/*
+ * Referenced https://github.com/esp8266/Arduino/blob/master/cores/esp8266/Print.cpp
+ * Preformat string then put into a buffer
+ */
+int serial_print(const char* format, ...)
+{
+	va_list arg;
+	char temp[PRINTF_BUFFER_LEN];
+	size_t len;
+	char* buffer = temp;
+
+	/* Format Variadic Args into string */
+	va_start(arg, format);
+	len = vsnprintf(temp, PRINTF_BUFFER_LEN, format, arg);
+	va_end(arg);
+
+	/* Check to make sure we don't overflow buffer */
+	if (len > PRINTF_BUFFER_LEN - 1)
+		return -1;
+
+	osMessageQueuePut(printf_queue, &buffer, 0U, 0U);
+	return 0;
+}
+
+void vSerialMonitor(void* pv_params)
+{
+	char* message;
+	osStatus_t status;
+
+	printf_queue = osMessageQueueNew(PRINTF_QUEUE_SIZE, sizeof(char*), NULL);
+
+	for (;;) {
+		/* Wait until new printf message comes into queue */
+		status = osMessageQueueGet(printf_queue, &message, NULL, 0U);
+		if (status != osOK) {
+			// TODO: Trigger fault ?
+		} else {
+			printf(message);
+		}
+	}
+}
