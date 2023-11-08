16f071887cb34493e22cfe59b71af7247436887a
Rand-Sai <yuan.sa@northeastern.edu>
2023-11-08 07:57:03 -0500

    fault handler attempt 2

--- Core/Src/fault.c
+++ Core/Src/fault.c
@@ -2,42 +2,35 @@
 
 osMessageQueueId_t fault_handle_queue;
 
-//Function to determine priority base on severity
-void priority(void) {
-    osThreadId_t id;
-    MSGQUEUE_OBJ_t msg;
+osThreadId_t fault_handle;
+const osThreadAttr_t fault_handle_attributes = {
+	.name		= "FaultHandler",
+	.stack_size = 128 * 4,
+	//.priority	= (osPriority_t)osPriorityBelowNormal3,
+};
+
+//Function assign a priority and tell the kernel to schedule on boot
+void vFaultHandler(void* pv_params) {
+    fault_data_t fault_data;
     osStatus_t status;
-    // Wait until a message is in the queue, send messages when they are in the queue
+    //Wait until a message is in the queue, send messages when they are in the queue
 	for(;;) {
-        status = osMessageQueueGet(fault_handle_queue, &msg, NULL, 0U);   // wait for message
-        id = osThreadGetId(); // Obtain ID of current running thread
+        status = osMessageQueueGet(fault_handle_queue, &fault_data, NULL, 0U);   // wait for message
         if (status == osOK) {
         
         // process data
-        switch (fault_sev_t) {
+        switch (fault_data.severity) {
         case 'DEFCON1': //Higest(1st) Priority
-            osThreadSetPriority(id, osPriorityHigh);
-            osThreadSetPriority(id, osPriorityHigh);
             break;
         case 'DEFCON2': //2nd Highest Priority
-            osThreadSetPriority(id, osPriorityAboveNormal);
-            osThreadSetPriority(id, osPriorityAboveNormal);
             break;
         case 'DEFCON3': //3rd Highest Priority
-            osThreadSetPriority(id, osPriorityNormal);
-            osThreadSetPriority(id, osPriorityNormal);
             break;
         case 'DEFCON4': //Slight Above Lowest Priority
-            osThreadSetPriority(id, osPriorityBelowNormal);
-            osThreadSetPriority(id, osPriorityBelowNormal);
             break;
         case 'DEFCON5': //Lowest Priority
-            osThreadSetPriority(id, osPriorityLow);
-            osThreadSetPriority(id, osPriorityLow);
             break;
         default: //Unable To Identify 'fault_sev_t'
-            osThreadSetPriority(id, osPriorityIdle);
-            osThreadSetPriority(id, osPriorityIdle);
             break;
             }
         }
