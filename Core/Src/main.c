190c4e04ebdf2a0fe6a12f0938dd531097ee666b
Hamza <hamza.n.iqbal@gmail.com>
2024-01-21 20:41:14 -0500

    Made reciever build

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -175,7 +175,7 @@ int main(void)
   dti_t *mc   = dti_init();
   //steeringio_t *wheel = steeringio_init();
   can_t *can1 = init_can1(&hcan1);
-
+  can1 = can1;
   /* USER CODE END RTOS_MUTEX */
 
   /* USER CODE BEGIN RTOS_SEMAPHORES */
@@ -209,7 +209,7 @@ int main(void)
   /* Note that CAN Router initializes CAN */
   dti_router_handle = osThreadNew(vDTIRouter, mc, &dti_router_attributes);
   // steeringio_router_handle = osThreadNew(vSteeringIORouter, wheel, &steeringio_router_attributes);
-  can_dispatch_handle = osThreadNew(vCanDispatch, can1, &can_dispatch_attributes);
+  // can_dispatch_handle = osThreadNew(vCanDispatch, can1, &can_dispatch_attributes);
   serial_monitor_handle = osThreadNew(vSerialMonitor, NULL, &serial_monitor_attributes);
 
   /* Control Logic */
