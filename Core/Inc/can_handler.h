203331d51f0f8bebd0ce7830de8f767f6f211a22
caiodasilva2005 <caiodasilva05@outlook.com>
2024-03-20 09:24:07 -0400

    Added bms can monitor to makefile

--- Core/Inc/can_handler.h
+++ Core/Inc/can_handler.h
@@ -25,10 +25,6 @@ void vCanDispatch(void* pv_params);
 extern osThreadId_t can_dispatch_handle;
 extern const osThreadAttr_t can_dispatch_attributes;
 
-void vBMSCANMonitor(void* pv_params);
-extern osThreadId_t bms_can_monitor_handle;
-extern const osThreadAttr_t bms_can_monitor_attributes;
-
 int8_t queue_can_msg(can_msg_t msg);
 can_t* init_can1(CAN_HandleTypeDef* hcan);
 
