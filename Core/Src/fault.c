92e41454f5a061a0240cd7a8de3c86b3dd0f2f66
Scott A <89099102+Sabramz@users.noreply.github.com>
2024-04-27 17:44:59 -0400

    submod update, sending faults on can, skibidi toilet v2

--- Core/Src/fault.c
+++ Core/Src/fault.c
@@ -4,6 +4,8 @@
 #include <assert.h>
 #include <stdio.h>
 #include "state_machine.h"
+#include "can_handler.h"
+#include <string.h>
 
 #define FAULT_HANDLE_QUEUE_SIZE 16
 
@@ -36,6 +38,16 @@ void vFaultHandler(void* pv_params)
 		/* Wait until a message is in the queue, send messages when they are in the queue */
 		status = osMessageQueueGet(fault_handle_queue, &fault_data, NULL, osWaitForever);
 		if (status == osOK) {
+
+			uint8_t fault_code_high = (fault_data.id & 0x0000FF00) >> 8;
+			uint8_t fault_code_low = (fault_data.id & 0x000000FF);
+
+			can_msg_t msg;
+			msg.id = CANID_FAULT_MSG;
+			msg.len = 8;
+			uint8_t msg_data[8] = {fault_code_high, fault_code_low};
+			memcpy(msg.data, msg_data, msg.len);
+			queue_can_msg(msg);
 			printf("\r\nFault Handler! Diagnostic Info:\t%s\r\n\r\n", fault_data.diag);
 			switch (fault_data.severity)
 			{
