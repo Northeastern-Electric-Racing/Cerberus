426940fbecd2c8c4bce3999f590091b92b1a51e3
Scott A <89099102+Sabramz@users.noreply.github.com>
2024-05-24 14:57:59 -0400

    add dti router, disable preemption

--- Core/Src/dti.c
+++ Core/Src/dti.c
@@ -24,7 +24,7 @@
 #define CAN_QUEUE_SIZE 5 /* messages */
 #define SAMPLES 20
 static osMutexAttr_t dti_mutex_attributes;
-// osMessageQueueId_t dti_router_queue;
+osMessageQueueId_t dti_router_queue;
 
 dti_t* dti_init()
 {
