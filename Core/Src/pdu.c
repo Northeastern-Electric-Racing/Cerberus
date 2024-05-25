cf35e39b245284198abc030c9bb2d5b745fe778e
Jack Rubacha <rubacha.jack03@gmail.com>
2024-05-25 01:03:16 -0400

    switch to pca9539 (untested)

--- Core/Src/pdu.c
+++ Core/Src/pdu.c
@@ -1,8 +1,8 @@
 #include "pdu.h"
 #include "serial_monitor.h"
 #include <assert.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define PUMP_CTRL	   0
 #define RADFAN_CTRL	   1
@@ -14,28 +14,28 @@
 #define RTDS_CTRL	   15
 #define MUTEX_TIMEOUT  osWaitForever /* ms */
 
-#define SHUTDOWN_ADDR  0x20
-#define CTRL_ADDR      0x24
-#define RTDS_DURATION	2500
+#define SHUTDOWN_ADDR PCA_I2C_ADDR_0
+#define CTRL_ADDR	  PCA_I2C_ADDR_1
+#define RTDS_DURATION 2500
 
 #define PIN_MODE_OUTPUT 0
-#define PIN_MODE_INPUT  1
+#define PIN_MODE_INPUT	1
 
 static osMutexAttr_t pdu_mutex_attributes;
 
 static void rtds_shutoff_cb(void* pv_params)
 {
-	pdu_t *pdu = (pdu_t *)pv_params;
+	pdu_t* pdu		= (pdu_t*)pv_params;
 	osStatus_t stat = osMutexAcquire(pdu->mutex, MUTEX_TIMEOUT);
 	if (stat)
 		return;
 
 	/* write RTDS over i2c */
-    HAL_StatusTypeDef error = max7314_set_pin_state(pdu->ctrl_expander, RTDS_CTRL, false);
-    if(error != HAL_OK) {
-        osMutexRelease(pdu->mutex);
-        return;
-    }
+	HAL_StatusTypeDef error = pca9539_write_pin(pdu->ctrl_expander, PCA_OUTPUT, PCA_B1, RTDS_CTRL, false);
+	if (error != HAL_OK) {
+		osMutexRelease(pdu->mutex);
+		return;
+	}
 
 	osMutexRelease(pdu->mutex);
 }
@@ -51,55 +51,50 @@ pdu_t* init_pdu(I2C_HandleTypeDef* hi2c)
 	pdu->hi2c = hi2c;
 
 	/* Initialize Shutdown GPIO Expander */
-	pdu->shutdown_expander = malloc(sizeof(max7314_t));
-    assert(pdu->shutdown_expander);
-    pdu->shutdown_expander->dev_addr = SHUTDOWN_ADDR;
-	max7314_init(pdu->shutdown_expander, pdu->hi2c);
+	pdu->shutdown_expander = malloc(sizeof(pca9539_t));
+	assert(pdu->shutdown_expander);
+	pca9539_init(pdu->shutdown_expander, pdu->hi2c, SHUTDOWN_ADDR);
 
-	// Blink disabled, global intensity enabled
-	uint8_t config_data = 0b01001100;
-    HAL_StatusTypeDef status = max7314_write_config(pdu->shutdown_expander, &config_data);
-    if (status != HAL_OK) {
-        printf("\n\rshutdown init fail\n\r");
-	    free(pdu->shutdown_expander);
-	    free(pdu);
-	    return NULL;
-    }
+	// all shutdown expander things are inputs
+	bool shutdown_config_directions[8] = { 0, 0, 0, 0, 0, 0, 0, 0 } ;
+	HAL_StatusTypeDef status = pca9539_write_pins(pdu->shutdown_expander, PCA_DIRECTION, PCA_B0, shutdown_config_directions);
+	if (status != HAL_OK) {
+		printf("\n\rshutdown init fail\n\r");
+		free(pdu->shutdown_expander);
+		free(pdu);
+		return NULL;
+	}
+	status
+		= pca9539_write_pins(pdu->shutdown_expander, PCA_DIRECTION, PCA_B1, shutdown_config_directions);
+	if (status != HAL_OK) {
+		printf("\n\rshutdown init fail\n\r");
+		free(pdu->shutdown_expander);
+		free(pdu);
+		return NULL;
+	}
 
 	/* Initialize Control GPIO Expander */
-	pdu->ctrl_expander = malloc(sizeof(max7314_t));
-    assert(pdu->ctrl_expander);
-    pdu->ctrl_expander->dev_addr = CTRL_ADDR;
-	max7314_init(pdu->ctrl_expander, pdu->hi2c);
+	pdu->ctrl_expander = malloc(sizeof(pca9539_t));
+	assert(pdu->ctrl_expander);
+	pca9539_init(pdu->ctrl_expander, pdu->hi2c, CTRL_ADDR);
 
-	/* Same as shutdown */
-	status = max7314_write_config(pdu->ctrl_expander, &config_data);
-    if (status != HAL_OK) {
-        printf("\n\rcntrl init fail\n\r");
-	    free(pdu->shutdown_expander);
-	    free(pdu);
-	    return NULL;
-    }
 
-	/* Set global intensity */
-	status = max7314_set_global_intensity(pdu->ctrl_expander, 0);
-    if (status != HAL_OK) {
-        printf("\n\rSet global intensity fail\n\r");
+	bool control_config_directions_0[8] = { 1, 1, 1, 1, 0, 0, 0, 0 };
+	status = pca9539_write_pins(pdu->ctrl_expander, PCA_DIRECTION, PCA_B0, control_config_directions_0);
+	if (status != HAL_OK) {
+		printf("\n\rcntrl init fail\n\r");
 		free(pdu->ctrl_expander);
-	    free(pdu->shutdown_expander);
-	    free(pdu);
-	    return NULL;
-    }
-
-    // set pins 15, 3, 2, 1, 0 to outputs
-	uint8_t pin_config[2] = {0b11110000, 0b01111111};
-    if (max7314_set_pin_modes(pdu->ctrl_expander, pin_config)) {
-		printf("\n\rset pin modes fail\n\r");
-        free(pdu->ctrl_expander);
-        free(pdu->shutdown_expander);
-        free(pdu);
-        return NULL;
-    }
+		free(pdu);
+		return NULL;
+	}
+	bool control_config_directions_1[8] = { 0, 0, 0, 0, 0, 0, 0, 1 };
+	status = pca9539_write_pins(pdu->ctrl_expander, PCA_DIRECTION, PCA_B1, control_config_directions_1);
+	if (status != HAL_OK) {
+		printf("\n\rcntrl init fail\n\r");
+		free(pdu->ctrl_expander);
+		free(pdu);
+		return NULL;
+	}
 
 	/* Create Mutex */
 	pdu->mutex = osMutexNew(&pdu_mutex_attributes);
@@ -107,16 +102,16 @@ pdu_t* init_pdu(I2C_HandleTypeDef* hi2c)
 
 	pdu->rtds_timer = osTimerNew(&rtds_shutoff_cb, osTimerOnce, pdu, NULL);
 
-	assert(!max7314_set_pin_state(pdu->ctrl_expander, RTDS_CTRL, false));
+	//assert(!max7314_set_pin_state(pdu->ctrl_expander, RTDS_CTRL, false));
 
 	// DEBUG To test RTDS
-	//sound_rtds(pdu);
+	// sound_rtds(pdu);
 
 	// DEBUG brakelight
-	//write_brakelight(pdu, true);
+	// write_brakelight(pdu, true);
 
 	// DEBUG pump
-	//write_pump(pdu, false);
+	// write_pump(pdu, false);
 
 	return pdu;
 }
@@ -133,11 +128,11 @@ int8_t write_pump(pdu_t* pdu, bool status)
 	}
 
 	/* write pump over i2c */
-    HAL_StatusTypeDef error = max7314_set_pin_state(pdu->ctrl_expander, PUMP_CTRL, status);
-    if(error != HAL_OK) {
-        osMutexRelease(pdu->mutex);
-        return error;
-    }
+	HAL_StatusTypeDef error = pca9539_write_pin(pdu->ctrl_expander, PCA_OUTPUT, PCA_B0, PUMP_CTRL, status);
+	if (error != HAL_OK) {
+		osMutexRelease(pdu->mutex);
+		return error;
+	}
 
 	osMutexRelease(pdu->mutex);
 	return 0;
@@ -153,11 +148,11 @@ int8_t write_fault(pdu_t* pdu, bool status)
 		return stat;
 
 	/* write radiator over i2c */
-    HAL_StatusTypeDef error = max7314_set_pin_state(pdu->ctrl_expander, RADFAN_CTRL, status);
-    if(error != HAL_OK) {
-        osMutexRelease(pdu->mutex);
-        return error;
-    }
+	HAL_StatusTypeDef error = pca9539_write_pin(pdu->ctrl_expander, PCA_OUTPUT, PCA_B0, RADFAN_CTRL, status);
+	if (error != HAL_OK) {
+		osMutexRelease(pdu->mutex);
+		return error;
+	}
 
 	osMutexRelease(pdu->mutex);
 	return 0;
@@ -173,11 +168,11 @@ int8_t write_brakelight(pdu_t* pdu, bool status)
 		return stat;
 
 	/* write brakelight over i2c */
-    HAL_StatusTypeDef error = max7314_set_pin_state(pdu->ctrl_expander, BRKLIGHT_CTRL, status);
-    if(error != HAL_OK) {
-        osMutexRelease(pdu->mutex);
-        return error;
-    }
+	HAL_StatusTypeDef error = pca9539_write_pin(pdu->ctrl_expander, PCA_OUTPUT, PCA_B0, BRKLIGHT_CTRL, status);
+	if (error != HAL_OK) {
+		osMutexRelease(pdu->mutex);
+		return error;
+	}
 
 	osMutexRelease(pdu->mutex);
 	return 0;
@@ -193,11 +188,11 @@ int8_t write_fan_battbox(pdu_t* pdu, bool status)
 		return stat;
 
 	/* write fan over i2c */
-    HAL_StatusTypeDef error = max7314_set_pin_state(pdu->ctrl_expander, BATBOXFAN_CTRL, status);
-    if(error != HAL_OK) {
-        osMutexRelease(pdu->mutex);
-        return error;
-    }
+	HAL_StatusTypeDef error = pca9539_write_pin(pdu->ctrl_expander, PCA_OUTPUT, PCA_B0, BATBOXFAN_CTRL, status);
+	if (error != HAL_OK) {
+		osMutexRelease(pdu->mutex);
+		return error;
+	}
 
 	osMutexRelease(pdu->mutex);
 	return 0;
@@ -205,7 +200,7 @@ int8_t write_fan_battbox(pdu_t* pdu, bool status)
 
 int8_t sound_rtds(pdu_t* pdu)
 {
-    if (!pdu)
+	if (!pdu)
 		return -1;
 
 	osStatus_t stat = osMutexAcquire(pdu->mutex, MUTEX_TIMEOUT);
@@ -215,18 +210,18 @@ int8_t sound_rtds(pdu_t* pdu)
 	osTimerStart(pdu->rtds_timer, RTDS_DURATION);
 
 	/* write RTDS over i2c */
-    HAL_StatusTypeDef error = max7314_set_pin_state(pdu->ctrl_expander, RTDS_CTRL, true);
-    if(error != HAL_OK) {
-        osMutexRelease(pdu->mutex);
-        return error;
-    }
+	HAL_StatusTypeDef error = pca9539_write_pin(pdu->ctrl_expander, PCA_OUTPUT, PCA_B1, RTDS_CTRL, true);
+	if (error != HAL_OK) {
+		osMutexRelease(pdu->mutex);
+		return error;
+	}
 
 	osMutexRelease(pdu->mutex);
 
 	return 0;
 }
 
-int8_t read_fuse(pdu_t* pdu, fuse_t fuse, bool* status)
+int8_t read_fuses(pdu_t* pdu, bool status[MAX_FUSES])
 {
 	if (!pdu)
 		return -1;
@@ -235,45 +230,29 @@ int8_t read_fuse(pdu_t* pdu, fuse_t fuse, bool* status)
 	if (stat)
 		return stat;
 
-	uint16_t pin;
-	switch (fuse)
-	{
-		case FUSE_BATTBOX:
-			pin = 4;
-			break;
-		case FUSE_LVBOX:
-			pin = 5;
-			break;
-		case FUSE_FAN_RADIATOR:
-			pin = 6;
-			break;
-		case FUSE_MC:
-			pin = 7;
-			break;
-		case FUSE_FAN_BATTBOX:
-			pin = 8;
-			break;
-		case FUSE_PUMP:
-			pin = 9;
-			break;
-		case FUSE_DASHBOARD:
-			pin = 10;
-			break;
-		case FUSE_BRAKELIGHT:
-			pin = 11;
-			break;
-		case FUSE_BRB:
-			pin = 12;
-			break;
-		default:
-			return -1;
+	bool bank0[8];
+	HAL_StatusTypeDef error = pca9539_read_pins(pdu->ctrl_expander, PCA_INPUT, PCA_B0, bank0);
+	if (error != HAL_OK) {
+		osMutexRelease(pdu->mutex);
+		return error;
 	}
 
-	HAL_StatusTypeDef error = max7314_read_pin(pdu->ctrl_expander, pin, status);
-    if(error != HAL_OK) {
-        osMutexRelease(pdu->mutex);
-        return error;
-    }
+	bool bank1[8];
+	error = pca9539_read_pins(pdu->ctrl_expander, PCA_INPUT, PCA_B1, bank1);
+	if (error != HAL_OK) {
+		osMutexRelease(pdu->mutex);
+		return error;
+	}
+
+	status[FUSE_BATTBOX] = bank0[4];
+	status[FUSE_LVBOX] = bank0[5];
+	status[FUSE_FAN_RADIATOR] = bank0[6];
+	status[FUSE_MC] = bank0[7];
+	status[FUSE_FAN_BATTBOX] = bank1[0];
+	status[FUSE_PUMP] = bank1[1];
+	status[FUSE_DASHBOARD] = bank1[2];
+	status[FUSE_BRAKELIGHT] = bank1[3];
+	status[FUSE_BRB] = bank1[4];
 
 	osMutexRelease(pdu->mutex);
 	return 0;
@@ -289,18 +268,18 @@ int8_t read_tsms_sense(pdu_t* pdu, bool* status)
 		return stat;
 
 	/* read pin over i2c */
-    uint8_t tsms_pin = 14;
-    HAL_StatusTypeDef error = max7314_read_pin(pdu->ctrl_expander, tsms_pin, status);
-    if(error != HAL_OK) {
-        osMutexRelease(pdu->mutex);
-        return error;
-    }
+	uint8_t tsms_pin		= 14;
+	HAL_StatusTypeDef error = pca9539_read_pin(pdu->ctrl_expander, PCA_INPUT, PCA_B1, tsms_pin, status);
+	if (error != HAL_OK) {
+		osMutexRelease(pdu->mutex);
+		return error;
+	}
 
 	osMutexRelease(pdu->mutex);
 	return 0;
 }
 
-int8_t read_shutdown(pdu_t* pdu, shutdown_stage_t stage, bool* status)
+int8_t read_shutdown(pdu_t* pdu, bool status[MAX_SHUTDOWN_STAGES])
 {
 	if (!pdu)
 		return -1;
@@ -309,54 +288,37 @@ int8_t read_shutdown(pdu_t* pdu, shutdown_stage_t stage, bool* status)
 	if (stat)
 		return stat;
 
-	uint16_t pin;
-	switch (stage)
-	{
-		case CKPT_BRB_CLR:
-			pin = 0;
-			break;
-		case BMS_OK:
-			pin = 2;
-			break;
-		case INERTIA_SW_OK:
-			pin = 3;
-			break;
-		case SPARE_GPIO1_OK:
-			pin = 4;
-			break;
-		case IMD_OK:
-			pin = 5;
-			break;
-		case BSPD_OK:
-			pin = 8;
-			break;
-		case BOTS_OK:
-			pin = 13;
-			break;
-		case HVD_INTLK_OK:
-			pin = 14;
-			break;
-		case HVC_INTLK_OK:
-			pin = 15;
-			break;
-		default:
-			return -1;
+	bool bank0[8];
+	HAL_StatusTypeDef error = pca9539_read_pins(pdu->shutdown_expander, PCA_INPUT, PCA_B0, bank0);
+	if (error != HAL_OK) {
+		osMutexRelease(pdu->mutex);
+		return error;
 	}
 
-	// read pin over i2c
-    HAL_StatusTypeDef error = max7314_read_pin(pdu->shutdown_expander, pin, status);
-    if(error != HAL_OK) {
-        osMutexRelease(pdu->mutex);
-        return error;
-    }
+	bool bank1[8];
+	error = pca9539_read_pins(pdu->shutdown_expander, PCA_INPUT, PCA_B1, bank1);
+	if (error != HAL_OK) {
+		osMutexRelease(pdu->mutex);
+		return error;
+	}
+
+	status[CKPT_BRB_CLR] = bank0[0];
+	status[BMS_OK] = bank0[2];
+	status[INERTIA_SW_OK] = bank0[3];
+	status[SPARE_GPIO1_OK] = bank0[4];
+	status[IMD_OK] = bank0[5];
+	status[BSPD_OK] = bank1[0];
+	status[BOTS_OK] = bank1[5];
+	status[HVD_INTLK_OK] = bank1[6];
+	status[HVC_INTLK_OK] = bank1[7];
 
 	osMutexRelease(pdu->mutex);
 	return 0;
 }
 
-int8_t write_rtds(pdu_t* pdu, bool status) 
+int8_t write_rtds(pdu_t* pdu, bool status)
 {
-    if (!pdu)
+	if (!pdu)
 		return -1;
 
 	osStatus_t stat = osMutexAcquire(pdu->mutex, MUTEX_TIMEOUT);
@@ -364,11 +326,11 @@ int8_t write_rtds(pdu_t* pdu, bool status)
 		return stat;
 
 	/* write fan over i2c */
-    HAL_StatusTypeDef error = max7314_set_pin_state(pdu->ctrl_expander, RTDS_CTRL, status);
-    if(error != HAL_OK) {
-        osMutexRelease(pdu->mutex);
-        return error;
-    }
+	HAL_StatusTypeDef error = pca9539_write_pin(pdu->ctrl_expander, PCA_OUTPUT, PCA_B1, RTDS_CTRL, status);
+	if (error != HAL_OK) {
+		osMutexRelease(pdu->mutex);
+		return error;
+	}
 
 	osMutexRelease(pdu->mutex);
 	return 0;
