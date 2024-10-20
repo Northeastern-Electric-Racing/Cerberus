f59f74ba04dcd654ac62e74b60b3f3d4d20e8610
marvin-g-neu <141779828+marvin-g-neu@users.noreply.github.com>
2024-10-20 16:43:54 -0400

    fixed errors

--- Core/Src/mpu.c
+++ Core/Src/mpu.c
@@ -153,8 +153,8 @@ int8_t read_accel(mpu_t *mpu, uint16_t accel[3])
 	if (mut_stat)
 		return mut_stat;
 
-    HAL_StatusTypeDef hal_stat = lsm6dso_read_accel(mpu->imu);
-	if (hal_stat)
+	HAL_StatusTypeDef hal_stat = lsm6dso_read_accel(mpu->imu);
+	if (hal_stat != HAL_OK)
 		return hal_stat;
 	memcpy(accel, mpu->imu->accel_data, 3);
 
