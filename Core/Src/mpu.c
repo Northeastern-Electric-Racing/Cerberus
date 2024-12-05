33a676d1f9167626d70f59e4d50d12908bb57db9
Jake-Hensley <hensley.j@northeastern.edu>
2024-12-04 19:17:51 -0500

    changed to watchdog

--- Core/Src/mpu.c
+++ Core/Src/mpu.c
@@ -192,6 +192,6 @@ int8_t read_gyro(mpu_t *mpu)
 int8_t write_fault(mpu_t *mpu, bool status)
 {
 	if (!mpu)
-		HAL_GPIO_WritePin(mpu->fault_gpio, CAN_FAULT_PIN, status);
+		HAL_GPIO_WritePin(mpu->watchdog_gpio, CAN_FAULT_PIN, status);
 	return 0;
 }
