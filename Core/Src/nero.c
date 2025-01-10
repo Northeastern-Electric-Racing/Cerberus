46da9de621afd2f56e16d32455fe615df11214f8
Myers-Ty <myers.ty@northeastern.edu>
2025-01-10 12:01:41 -0500

    formatting

--- Core/Src/nero.c
+++ Core/Src/nero.c
@@ -1,17 +1,17 @@
 #include "nero.h"
-#include "stdint.h"
-#include "stdbool.h"
-#include "can_handler.h"
-#include "state_machine.h"
-#include "queues.h"
 #include "c_utils.h"
-#include "stdio.h"
+#include "can_handler.h"
 #include "cerberus_conf.h"
 #include "monitor.h"
 #include "pedals.h"
+#include "queues.h"
+#include "state_machine.h"
+#include "stdbool.h"
+#include "stdint.h"
+#include "stdio.h"
 #include "string.h"
 
-//#define TORQUE_DEBUG
+// #define TORQUE_DEBUG
 
 static int8_t mph = 0;
 
@@ -25,7 +25,8 @@ void send_nero_msg()
 		uint8_t torque_lim_percentage;
 	} nero_data;
 
-	/* Since the screen on NERO relies on the NERO index, and reverse and pit have the same index, reverse gets a special index */
+	/* Since the screen on NERO relies on the NERO index, and reverse and pit have the same index,
+	 * reverse gets a special index */
 	if (get_func_state() == REVERSE) {
 		nero_data.nero_index = 255;
 	} else {
@@ -33,21 +34,20 @@ void send_nero_msg()
 	}
 
 	nero_data.home_mode = (uint8_t)get_nero_state().home_mode;
-	nero_data.mph = mph;
-	nero_data.tsms = (uint8_t)get_tsms();
+	nero_data.mph		= mph;
+	nero_data.tsms		= (uint8_t)get_tsms();
 	/* Percentage from 0 - 1, multiplied by 100 */
-	nero_data.torque_lim_percentage =
-		(uint8_t)(get_torque_limit_percentage() * 100);
+	nero_data.torque_lim_percentage = (uint8_t)(get_torque_limit_percentage() * 100);
 
 	can_msg_t msg = { .id = 0x501, .len = sizeof(nero_data) };
 
 	memcpy(&msg.data, &nero_data, sizeof(nero_data));
 
-	#ifdef TORQUE_DEBUG
-		nero_data.tsms = 1;
-		nero_data.nero_index = 1; /* Used for selecting drive mode */
-		dti_set_current(0);		
-	#endif
+#ifdef TORQUE_DEBUG
+	nero_data.tsms		 = 1;
+	nero_data.nero_index = 1; /* Used for selecting drive mode */
+	dti_set_current(0);
+#endif
 
 	/* Send CAN message */
 	queue_can_msg(msg);
