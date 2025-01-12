5f6f3c9062d89b56a746827baa0b95ce4bb12959
Jake-Hensley <hensley.j@northeastern.edu>
2025-01-12 17:34:53 -0500

    switched to jack approved method

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -696,35 +696,38 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-struct __attribute__((__packed__)) {
+struct __attribute__((__packed__)) git_version_data {
 		uint8_t git_major_version;
 		uint8_t git_minor_version;
 		uint8_t git_patch_version;
 		bool git_is_upstream_clean;
 		bool git_is_local_clean;
-	} git_version_data = {
+	} git_version_data;
+  /* = {
     .git_major_version = GIT_MAJOR_VERSION,
     .git_minor_version = GIT_MINOR_VERSION,
     .git_patch_version = GIT_PATCH_VERSION,
     .git_is_upstream_clean = GIT_IS_UPSTREAM_CLEAN,
     .git_is_local_clean = GIT_IS_LOCAL_CLEAN,
-  };
-  struct __attribute__((__packed__)) {
+  };*/
+  struct __attribute__((__packed__)) git_hash_data {
     uint32_t git_shorthash;
     uint32_t git_authorhash;
-  } git_hash_data = {
+  } git_hash_data;
+  /* = {
     .git_shorthash = GIT_SHORTHASH,
     .git_authorhash = GIT_AUTHORHASH,
-  };
+  };*/
 
 //put funtion that makes can message and queues message, send_nero_message is similar, use variables from versioning over can ticket in embedded-base
 void send_git_version_message() {
-  //const struct git_hash_data git_hash_data_data = {GIT_SHORTHASH , GIT_AUTHORHASH};
-  can_msg_t msg1 = { .id = 0x698, .len = sizeof(git_version_data)};
-  can_msg_t msg2 = { .id = 0x699, .len = sizeof(git_hash_data)};
+  const struct git_hash_data git_hash_data2 = {GIT_SHORTHASH , GIT_AUTHORHASH};
+  const struct git_version_data git_version_data2 = {GIT_MAJOR_VERSION , GIT_MINOR_VERSION, GIT_PATCH_VERSION, GIT_IS_UPSTREAM_CLEAN, GIT_IS_LOCAL_CLEAN};
+  can_msg_t msg1 = { .id = 0x698, .len = sizeof(git_version_data2)};
+  can_msg_t msg2 = { .id = 0x699, .len = sizeof(git_hash_data2)};
 
-  memcpy(&msg1.data, &git_version_data, sizeof(git_version_data));
-  memcpy(&msg2.data, &git_hash_data, sizeof(git_hash_data));
+  memcpy(&msg1.data, &git_version_data2, sizeof(git_version_data2));
+  memcpy(&msg2.data, &git_hash_data2, sizeof(git_hash_data2));
 
   queue_can_msg(msg1);
   queue_can_msg(msg2);
