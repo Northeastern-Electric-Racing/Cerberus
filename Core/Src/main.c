548dcab4f43ac33cdacdffd5a42a0ef616461f36
Jake-Hensley <hensley.j@northeastern.edu>
2025-01-12 17:13:58 -0500

    Added values to structs, commented out some buggy code to test

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -696,29 +696,30 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-static struct __attribute__((__packed__)) {
+struct __attribute__((__packed__)) {
 		uint8_t git_major_version;
 		uint8_t git_minor_version;
 		uint8_t git_patch_version;
 		bool git_is_upstream_clean;
 		bool git_is_local_clean;
-	} git_version_data;
-
-  static struct __attribute__((__packed__)) {
+	} git_version_data = {
+    .git_major_version = GIT_MAJOR_VERSION,
+    .git_minor_version = GIT_MINOR_VERSION,
+    .git_patch_version = GIT_PATCH_VERSION,
+    .git_is_upstream_clean = GIT_IS_UPSTREAM_CLEAN,
+    .git_is_local_clean = GIT_IS_LOCAL_CLEAN,
+  };
+  struct __attribute__((__packed__)) {
     uint32_t git_shorthash;
     uint32_t git_authorhash;
-  } git_hash_data;
+  } git_hash_data = {
+    .git_shorthash = GIT_SHORTHASH,
+    .git_authorhash = GIT_AUTHORHASH,
+  };
 
 //put funtion that makes can message and queues message, send_nero_message is similar, use variables from versioning over can ticket in embedded-base
 void send_git_version_message() {
-  git_version_data.git_major_version = GIT_MAJOR_VERSION;
-	git_version_data.git_minor_version = GIT_MINOR_VERSION;
-	git_version_data.git_patch_version = GIT_PATCH_VERSION;
-  git_version_data.git_is_upstream_clean = GIT_IS_UPSTREAM_CLEAN;
-	git_version_data.git_is_local_clean = GIT_IS_LOCAL_CLEAN;
-
-  git_hash_data.git_shorthash = GIT_SHORTHASH;
-  git_hash_data.git_authorhash = GIT_AUTHORHASH;
+  //const struct git_hash_data git_hash_data_data = {GIT_SHORTHASH , GIT_AUTHORHASH};
   can_msg_t msg1 = { .id = 0x698, .len = sizeof(git_version_data)};
   can_msg_t msg2 = { .id = 0x699, .len = sizeof(git_hash_data)};
 
