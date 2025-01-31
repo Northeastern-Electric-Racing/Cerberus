430b120c9e628daaca3b0eb061bd185bc932f4e1
Daniel <dnakhooda@gmail.com>
2025-01-30 21:06:34 -0500

    pump bits

--- Core/Src/control.c
+++ Core/Src/control.c
@@ -10,7 +10,9 @@ const osThreadAttr_t control_attributes = {
 
 static int fanBattBoxState = 0;
 
-static int pumpState = 0;
+static int pumpState0 = 0;
+
+static int pumpState1 = 0;
 
 void vControl(void *param)
 {
@@ -19,8 +21,8 @@ void vControl(void *param)
 	for (;;) {
 		write_fan_battbox(pdu, fanBattBoxState);
 
-		write_pump_0(pdu, pumpState);
-		write_pump_1(pdu, pumpState);
+		write_pump_0(pdu, pumpState0);
+		write_pump_1(pdu, pumpState1);
 
 		osDelay(1000);
 	}
@@ -38,8 +40,14 @@ void control_fanbattbox_record(can_msg_t msg)
 void control_pump_record(can_msg_t msg)
 {
 	if (msg.data[0] > 0) {
-		pumpState = 1;
+		pumpState0 = 1;
 	} else {
-		pumpState = 0;
+		pumpState0 = 0;
+	}
+
+	if (msg.data[1] > 0) {
+		pumpState1 = 1;
+	} else {
+		pumpState1 = 0;
 	}
 }
