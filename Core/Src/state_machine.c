1d0785e74895b3379eb6bcaa2e4fb7a9f3accf49
Caio <caiodasilva05@outlook.com>
2025-02-04 19:51:11 -0500

    ts off

--- Core/Src/state_machine.c
+++ Core/Src/state_machine.c
@@ -172,7 +172,7 @@ static int transition_nero_state(nero_state_t new_state, pdu_t *pdu, dti_t *mc,
 
 		/* TSMS and MPH = 0 to enter games */
 		if (new_state.nero_index == GAMES) {
-			if (get_tsms() && dti_get_mph(mc) <= 1) {
+			if (!get_tsms() && dti_get_mph(mc) <= 1) {
 				return 1;
 			}
 
