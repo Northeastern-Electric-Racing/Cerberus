d78c6343989aa6d1754bcbaf80ede231d2c91c68
Caio <caiodasilva05@outlook.com>
2025-03-09 16:15:31 -0400

    fix crash

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -168,8 +168,6 @@ int main(void)
   MX_IWDG_Init();
   MX_ADC2_Init();
   /* USER CODE BEGIN 2 */
-  /* I2C Detect */
-  uint8_t devices = 0u;
 
   /* Create Interfaces to Represent Relevant Hardware */
   mpu_t *mpu  = init_mpu(&hadc3, &hadc1);
