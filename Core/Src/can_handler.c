6294129d6b62fffefe947e85f869cd3861cc8b19
bjackson312006 <blakejackson312006@gmail.com>
2025-03-10 15:24:47 -0400

    Fixed thing

--- Core/Src/can_handler.c
+++ Core/Src/can_handler.c
@@ -67,7 +67,7 @@ void can1_callback(CAN_HandleTypeDef *hcan)
 
 	new_msg.len = rx_header.DLC;
 
-	if (rx_header.StdId == 0) {
+	if (rx_header.IDE == CAN_ID_EXT) {
 		// If the message has an extended CAN ID, save the message accordingly.
 		new_msg.id = rx_header.ExtId;
 		new_msg.id_is_extended = true;
