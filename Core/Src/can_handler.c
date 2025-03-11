8f2ac2c664e122da2ebfcd201de985308c4fdcc1
bjackson312006 <blakejackson312006@gmail.com>
2025-03-10 20:17:11 -0400

    Formatting

--- Core/Src/can_handler.c
+++ Core/Src/can_handler.c
@@ -73,7 +73,7 @@ void can1_callback(CAN_HandleTypeDef *hcan)
 	}
 
 	new_msg.len = rx_header.DLC;
-	
+
 	if (rx_header.IDE == CAN_ID_EXT) {
 		// If the message has an extended CAN ID, save the message accordingly.
 		new_msg.id = rx_header.ExtId;
