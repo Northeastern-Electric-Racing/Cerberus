5e2170050f2038efbfe4c332b422d71aca4b0d2c
bjackson312006 <blakejackson312006@gmail.com>
2025-03-23 19:04:29 -0400

    Updated to Rev 2

--- Core/Inc/pdu.h
+++ Core/Inc/pdu.h
@@ -30,29 +30,13 @@ typedef struct {
 pdu_t *init_pdu(I2C_HandleTypeDef *hi2c, ADC_HandleTypeDef *pump_sensors_adc);
 
 /* Functions to Control PDU */
-int8_t write_pump_0(pdu_t *pdu, bool state);
 int8_t write_pump_1(pdu_t *pdu, bool state);
-int8_t write_24V_12V_buck(pdu_t *pdu, bool state);
+int8_t write_pump_2(pdu_t *pdu, bool state);
 int8_t write_brakelight(pdu_t *pdu, bool state);
 int8_t write_fan_battbox(pdu_t *pdu, bool state);
 int8_t write_rtds(pdu_t *pdu, bool state);
-int8_t write_radfan_0(pdu_t *pdu, bool state);
 int8_t write_radfan_1(pdu_t *pdu, bool state);
-
-/* Function to Read the Status of Fuses from PDU */
-typedef enum {
-	PUMP_FUSE_STAT0,
-	SD_TO_BRB_FUSE,
-	LV_BOARDS_FUSE_STAT,
-	RADFAN_FUSE_STAT,
-	BATTBOX_FUSE_STAT,
-	BUCK_FUSE_STAT,
-	FANBATTBOX_STAT,
-	PUMP_FUSE_STAT1,
-	DASHBOARD_FUSE_STAT,
-	BRKLIGHT_FUSE_STAT,
-	MAX_FUSES
-} fuse_t;
+int8_t write_radfan_2(pdu_t *pdu, bool state);
 
 /**
  * @brief Read the status of the PDU fuses.
@@ -71,20 +55,6 @@ int8_t read_fuses(pdu_t *pdu, bitstream_t *bitstream);
  * @return int8_t Error code.
  */
 int8_t read_tsms_sense(pdu_t *pdu, bool *status);
-
-/* Functions to Read Status of Various Stages of Shutdown Loop */
-typedef enum {
-	HVD_GOOD,
-	HVC_GOOD,
-	BOTS_GOOD,
-	CKPT_BRB,
-	BMS_GOOD,
-	INERTIA_SW_GOOD,
-	SPARE_GPIO0,
-	IMD_GOOD,
-	BSPD_GOOD,
-	MAX_SHUTDOWN_STAGES
-} shutdown_stage_t;
 
 /**
  * @brief Read the status of the shutdown loop.
@@ -151,22 +121,22 @@ int8_t read_brake_state(pdu_t *pdu, bool *status);
 // clang-format off
 /* CTRL Expander */
 #define CTRL_ADDR		 			TCA_I2C_ADDR_0
-#define PIN_PUMP_CTRL_0		 		0 // P00
-#define PIN_PUMP_CTRL_1		 		1 // P01
-#define PIN_24V_12V_BUCK_CTRL		2 // P02
-#define PIN_BRKLIGHT_CTRL	 		3 // P03
-#define PIN_FANBATTBOX_CTRL	 		4 // P04
-#define PIN_BATTBOX_FUSE_STAT		5 // P05
-#define PIN_LV_BOARDS_FUSE_STAT		6 // P06
-#define PIN_RADFAN_FUSE_STAT		7 // P07
-#define PIN_BUCK_FUSE_STAT	 		0 // P10
-#define PIN_FANBATTBOX_FUSE_STAT 	1 // P11
-#define PIN_PUMP_FUSE_STAT0	 		2 // P12
+#define PIN_PUMP_CTRL_1		 		0 // P00
+#define PIN_PUMP_CTRL_2		 		1 // P01
+#define PIN_BRKLIGHT_CTRL			2 // P02
+#define PIN_FANBATTBOX_CTRL	 		3 // P03
+#define PIN_RTDS_CTRL 				4 // P04
+#define PIN_RADFAN_CTRL_1			5 // P05
+#define PIN_RADFAN_CTRL_2			6 // P06
+#define PIN_BATTBOX_FUSE_STAT		7 // P07
+#define PIN_LV_BOARDS_FUSE_STAT		0 // P10
+#define PIN_RADFAN_FUSE_STAT		1 // P11
+#define PIN_FANBATTBOX_FUSE_STAT	2 // P12
 #define PIN_DASHBOARD_FUSE_STAT	 	3 // P13
 #define PIN_BRKLIGHT_FUSE_STAT	 	4 // P14
 #define PIN_SD_TO_BRB_FUSE_STAT	 	5 // P15
 #define PIN_PUMP_FUSE_STAT1	 		6 // P16
-#define PIN_RTDS_CTRL		 		7 // P17
+#define PIN_PUMP_FUSE_STAT2		 	7 // P17
 
 /* Shutdown Expander */
 #define SHUTDOWN_ADDR	    TCA_I2C_ADDR_1
@@ -180,8 +150,8 @@ int8_t read_brake_state(pdu_t *pdu, bool *status);
 #define PIN_SHUTDOWN_07	    7 // P07 (X)
 #define PIN_SHUTDOWN_10	    0 // P10 (X)
 #define PIN_MC_STAT	    	1 // P11
-#define PIN_SPARE_MON	    2 // P12
-#define PIN_SPARE_FAULT	    3 // P13
+#define PIN_SPARE_IN	    2 // P12
+#define PIN_SHUTDOWN_13	    3 // P13
 #define PIN_TSMS_SENSE	    4 // P14
 #define PIN_BOTS_GOOD	    5 // P15
 #define PIN_HVD_INTLK_GOOD  6 // P16
--- Core/Src/control.c
+++ Core/Src/control.c
@@ -81,7 +81,7 @@ void vControl(void *params)
 
 	device_control_t pump0 = {
 		.pdu = pdu,
-		.control_func = write_pump_0,
+		.control_func = write_pump_1,
 		.upper_temp = PUMP_UPPER_MOTOR_TEMP,
 		.lower_temp = PUMP_LOWER_MOTOR_TEMP,
 		.device_type = DEVICE_PUMP0,
@@ -89,7 +89,7 @@ void vControl(void *params)
 
 	device_control_t radfan0 = {
 		.pdu = pdu,
-		.control_func = write_radfan_0,
+		.control_func = write_radfan_1,
 		.upper_temp = RADFAN_UPPER_MOTOR_TEMP,
 		.lower_temp = RADFAN_LOWER_MOTOR_TEMP,
 		.device_type = DEVICE_RADFAN0,
@@ -97,7 +97,7 @@ void vControl(void *params)
 
 	device_control_t pump1 = {
 		.pdu = pdu,
-		.control_func = write_pump_1,
+		.control_func = write_pump_2,
 		.upper_temp = PUMP_UPPER_CONTROLLER_TEMP,
 		.lower_temp = PUMP_LOWER_CONTROLLER_TEMP,
 		.device_type = DEVICE_PUMP1,
@@ -105,14 +105,14 @@ void vControl(void *params)
 
 	device_control_t radfan1 = {
 		.pdu = pdu,
-		.control_func = write_radfan_1,
+		.control_func = write_radfan_2,
 		.upper_temp = RADFAN_UPPER_CONTROLLER_TEMP,
 		.lower_temp = RADFAN_LOWER_CONTROLLER_TEMP,
 		.device_type = DEVICE_RADFAN1,
 	};
 
-	write_pump_0(pdu, false);
 	write_pump_1(pdu, false);
+	write_pump_2(pdu, false);
 
 	for (;;) {
 		uint16_t motor_temp;
