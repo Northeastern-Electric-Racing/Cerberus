87fa02a5efbe54200c4c05576975fe285a5914bb
Max <150200355+maxr21@users.noreply.github.com>
2025-03-24 19:25:09 -0400

    Bug/faulty lv sense message (#318)
    Co-authored-by: Caio <caiodasilva05@outlook.com>

--- Core/Src/monitor.c
+++ Core/Src/monitor.c
@@ -123,13 +123,13 @@ void read_lv_sense(void *arg)
 	fault_data_t fault_data = { .fault_index.non_crit_fault =
 					    LV_MONITOR_FAULT,
 				    .severity = NONCRITICAL };
-	can_msg_t lv_msg = { .id = CANID_LV_MONITOR, .len = 5, .data = { 0 } };
+	can_msg_t lv_msg = { .id = CANID_LV_MONITOR, .len = 3, .data = { 0 } };
 
 	uint16_t v_int;
-	uint32_t soc_int;
+	uint8_t soc_int;
 
 	struct __attribute__((__packed__)) {
-		uint32_t v;
+		uint16_t v;
 		uint8_t soc;
 	} lv_data;
 
@@ -146,7 +146,7 @@ void read_lv_sense(void *arg)
 		      (10000.0 / (10000.0 + 100000)) * 0.9963;
 
 	// get final voltage
-	v_int = (uint32_t)(v_dec * 10000.0);
+	v_int = (uint32_t)(v_dec * 100.0);
 
 	// Calculate SoC using logistic function
 	// - Normal charged voltage is 29.4V
